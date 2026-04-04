04ed4b7a2e6fa5539b3d913b16785634d7634fe9
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-05 00:39:59 +0600

    all page and route setup done

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import { Outlet } from "react-router";
 
 const Root = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
