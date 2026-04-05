54d8563c89de37b5c887d834fa194f27b631f1f7
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-05 20:00:23 +0600

    home page tamplate done

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div>
+      <p>Card of total summary of the App</p>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router";
 
 const Root = () => {
   return (
-    <div>
+    <div className="font-display">
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
