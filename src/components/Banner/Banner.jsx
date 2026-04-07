476e18b96b50f98031084e867a93847386441e11
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-07 23:14:25 +0600

    project running

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import heroImage from "./../../assets/hero.png";
 
 const Banner = () => {
   return (
-    <div>
-      <p>Banner</p>
+    <div className="max-w-6xl mx-auto mt-10">
+      <h1 className="text-5xl font-bold text-center">
+        We Build <br /> <span className="color-primary">Productive</span> Apps
+      </h1>
+      <p className="text-center text-gray-400">
+        At HERO.IO, we craft innovative apps designed to make everyday life
+        simpler, smarter, and more exciting.Our goal is to turn your ideas into
+        digital experiences that truly make an impact.
+      </p>
+      <div>
+        <button></button>
+      </div>
+      <img className="block mx-auto" src={heroImage} alt="" />
     </div>
   );
 };
